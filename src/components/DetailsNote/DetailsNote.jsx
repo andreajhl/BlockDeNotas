e72623ed5da8696b02c1eec32a9a0d5bd56d6242
Andreajhl <andreah29@famosos.com>
2021-10-21 03:43:53 -0300

    feat: :construction: componente details (en progreso)

---
+++ src/components/DetailsNote/DetailsNote.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import {detailsNote,editNote} from '../../actions/index'
+
+import swal from 'sweetalert';
+
+
+
+export default function DetailsNote({match}) {
+
+  const id= match.params.id
+
+  const history=useHistory()
+
+  const dispatch = useDispatch()
+  dispatch(detailsNote(id))
+
+  const state = useSelector(state => state.Detail)
+  
+  function culminar(){
+    dispatch(editNote(id))
+    swal("Tarea Culminada", "", "success")
+    history.push('/')
+   
+  }
+
+ if(state){
+    return (
+      <div>
+        <p>{state.title}</p>
+        <p>{state.status}</p>
+        <p>{state.description}</p>
+       {
+         state.status==='Activo' && <div>
+           <p>¿culminaste tu tarea?</p>
+           <button onClick={()=> culminar()}></button>
+         </div>
+       }
+        
+      </div>
+    )
+  }else{
+    setTimeout(() => {
+      history.push('/')
+    }, 1000);
+   
+    return(
+       <div>No existe la tarea buscada</div>
+    )
+    
+  }
+  
+};
