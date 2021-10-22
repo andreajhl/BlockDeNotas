7e6f8045134be1d53c7af654f4f3506dc2df4738
Andreajhl <andreah29@famosos.com>
2021-10-22 04:48:20 -0300

    feat: :construction: agrega nuevo componente card note (en progreso)

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -4,23 +4,36 @@ import { Link } from 'react-router-dom';
 
 import {removeNote, editNote} from '../../actions/index.js'
 
-import '../../Styles/Todo.scss'
+import '../../Styles/Note.scss'
 import {TiDelete} from 'react-icons/ti'
-import {AiFillCheckCircle} from 'react-icons/ai'
+import {FaPencilAlt} from 'react-icons/fa'
+import {BsEmojiSmile} from 'react-icons/bs'
 
-export default function Note({tod}) {
-  const dispatch = useDispatch()
-  console.log(tod)
+export default function Note({note}) {
+
+    const {id,title}=note;
+    const dispatch = useDispatch();
+
+    var colorNote;
+    var optionColor=`${id/3}`.split('.')
+    
+    if(optionColor[1] === undefined) colorNote = '#00A19D'
+    else if(optionColor[1][0]%2===0) colorNote = '#FFB344'
+    else colorNote = '#B1E693'
     return (
-    <div className='todo_c'>
-      <div>
-        {/* <div>
-          <button onClick={()=>dispatch(removeNote(tod.id))}><TiDelete /></button>
-          {tod.status !== 'Terminado' && <button onClick={()=>dispatch(editNote(tod.id))}><AiFillCheckCircle /></button>} 
-        </div> */}
-      </div>
-     {tod && <Link to={`/edit/${tod.id}`}><p>{tod.title}</p></Link>}
-    </div>
-  )
-  
+        <div className='note' style={{backgroundColor:colorNote}}>
+            <div className='note_pin'>
+                <BsEmojiSmile className='note_pin_i'/>
+            </div>
+            <div className='note_buttons'>
+                <button onClick={()=>dispatch(editNote(id))} className='note_buttons_element'><FaPencilAlt/></button>
+                <button onClick={()=>dispatch(removeNote(id))} className='note_buttons_element'><TiDelete className='note_buttons_i' /></button>
+            </div>
+            <div>
+                <Link to={`/edit/${id}`} key={id} style={{textDecoration:'none'}}>
+                    <p>{title}</p>
+                </Link>
+            </div>
+        </div>
+    )
 };
