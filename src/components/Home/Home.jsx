ce5c428e8e9e79f26ffcea8dee818147be8f78bf
Andreajhl <andreah29@famosos.com>
2021-10-24 03:27:51 -0300

    feat: :sparkles: agrega funcion de logout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { initialNotes } from '../../actions/index.js';
+import { initialNotes, logoutState} from '../../actions/index.js';
 
 import Paginacion from '../Paginacion/Paginacion.jsx';
 import DetailsNote from '../DetailsNote/DetailsNote.jsx';
 
-import '../../Styles/Home.scss'
-import details from '../../img/details.png'
+import '../../Styles/Home.scss';
+import {CgLogOut} from 'react-icons/cg';
+import {ImPencil2} from 'react-icons/im';
+import details from '../../img/details.png';
 
 const noteDetailsState=(state)=>state.details;
 
@@ -22,9 +25,13 @@ export function Home() {
 
   return (
     <div className='home'>
+      <button onClick={()=>dispatch(logoutState())} className='home_logout'><CgLogOut className='home_add_i'/></button>
      <div className='home_note_details'>
+        <Link to='/add' className='home_add'><ImPencil2 className='home_add_i'/> New Note</Link>
         <img src={details} className='home_note_details_i' />
-        {noteDetails && <DetailsNote />}
+        {noteDetails.title && <div className='home_note_details_text'>
+          <DetailsNote />
+        </div>}
       </div>
       <div className='home_notes'>
         <div className='home_notes_col'>
